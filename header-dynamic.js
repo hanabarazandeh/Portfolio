66964470f7bc55d30fcb3161c143ea005971a581
HanaBarazandeh <haniebarazande@gmail.com>
2021-08-06 23:05:58 -0700

    changed the dynamic header in a way that only nav bar is visible when scrolling down

---
+++ header-dynamic.js
@@ -0,0 +1,82 @@
+var onTopofPage = true;
+var header = document.querySelector("header");
+
+var headerInfo = document.querySelector(".header-info-container");
+var career = document.querySelector(".career");
+// var bodyHome = document.querySelector("body");
+
+var mainContainer = document.querySelector(".main-container");
+var dataVisContainer = document.querySelector("#data-vis-main-container");
+// var dataVisBody = document.querySelector(".body-data-vis");
+
+var isHomePage = false;
+
+// https://www.codegrepper.com/code-examples/javascript/get+current+html+file+name+javascript
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log(page);
+
+var scrollAmount;
+
+window.addEventListener("scroll", function() {
+    console.log(this.pageYOffset);
+
+    if (page == "about.html") {
+        scrollAmount = 170;
+    } else {
+        scrollAmount = 300;
+    }
+    if (this.pageYOffset > scrollAmount) {
+        //default classes
+        //hiding stuff
+        if (!header.classList.contains('header-smaller')) {
+            header.classList.add('header-fixed');
+            header.classList.add('header-smaller');
+
+
+            if (headerInfo != null)
+                headerInfo.setAttribute('aria-hidden', 'false');
+
+
+            header.classList.remove('add-gutters');
+            onTopofPage = false;
+            // bodyHome.classList.remove("body-home");
+            // dataVisBody.classList.remove("body-data-vis");
+            mainContainer.classList.add("top-margin");
+            if (dataVisContainer != null)
+
+                dataVisContainer.classList.add("bigger-top-margin");
+
+            //check if the current page is home page(index.html)
+            if (header.classList.contains('two-column')) {
+                header.classList.remove('two-column');
+                header.classList.remove('grid');
+                header.classList.remove('add-gutters');
+                isHomePage = true;
+
+            }
+
+        }
+    } else {
+        //unhiding stuff
+        // bodyHome.classList.add("body-home");
+        // dataVisBody.classList.add("body-data-vis");
+        header.classList.remove('header-smaller');
+        header.classList.remove('header-fixed');
+
+        if (headerInfo != null)
+            headerInfo.setAttribute('aria-hidden', 'true');
+
+        mainContainer.classList.remove("top-margin");
+        if (dataVisContainer != null)
+            dataVisContainer.classList.remove("bigger-top-margin");
+        if (isHomePage) {
+            header.classList.add('two-column');
+            header.classList.add('grid');
+            header.classList.add('add-gutters');
+        }
+    }
+
+});
+
+console.log("hi");
